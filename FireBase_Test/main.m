0bf707dfa96323c2edd5698ba9788791786e3032
guardabrazo <guardabrazo@MacBook-Pro-de-Federico.local>
2014-07-16 13:17:08 +0200

    Initial Commit

---
+++ FireBase_Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FireBase_Test
+//
+//  Created by Federico Guardabrazo Vallejo on 16/07/14.
+//  Copyright (c) 2014 guardabrazo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GBZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GBZAppDelegate class]));
+    }
+}
---
+++ FireBase_TestTests/FireBase_TestTests.m
@@ -0,0 +1,34 @@
+//
+//  FireBase_TestTests.m
+//  FireBase_TestTests
+//
+//  Created by Federico Guardabrazo Vallejo on 16/07/14.
+//  Copyright (c) 2014 guardabrazo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FireBase_TestTests : XCTestCase
+
+@end
+
+@implementation FireBase_TestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
