1d7764110e82ea668038ed1dd7e9c764079d5a42
guardabrazo <guardabrazo@MacBook-Pro-de-Federico.local>
2014-07-16 13:30:14 +0200

    Created FireBaseManager class

---
+++ FireBase_Test/GBZFireBaseManager.h
@@ -0,0 +1,21 @@
+//
+//  GBZFireBaseManager.h
+//  FireBaseTest
+//
+//  Created by Federico Guardabrazo Vallejo on 16/07/14.
+//  Copyright (c) 2014 guardabrazo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBZFireBaseManager : NSObject
+
+@property (nonatomic) NSUInteger numberOfPlayers;
+
+- (instancetype)initWithFireBaseRefAndPlayerID;
+
+- (void)addPlayer;
+
+- (void)startMonitoringNumberOfPlayers;
+
+@end
---
+++ FireBase_Test/GBZFireBaseManager.m
@@ -0,0 +1,62 @@
+//
+//  GBZFireBaseManager.m
+//  FireBaseTest
+//
+//  Created by Federico Guardabrazo Vallejo on 16/07/14.
+//  Copyright (c) 2014 guardabrazo. All rights reserved.
+//
+
+#define MECHANIMALS_FIREBASE_URL @"https://mechanimals.firebaseio.com/room/"
+
+#import "GBZFireBaseManager.h"
+#import <Firebase/Firebase.h>
+
+@interface GBZFireBaseManager ()
+
+@property (strong, nonatomic) Firebase *fireBaseRef;
+
+@property (copy, nonatomic) NSString *playerID;
+
+@end
+
+@implementation GBZFireBaseManager
+
+
+- (instancetype)initWithFireBaseRefAndPlayerID{
+    
+    self = [super init];
+    if (self) {
+        _fireBaseRef = [[Firebase alloc] initWithUrl:MECHANIMALS_FIREBASE_URL];
+        _playerID = [self createPlayerID];
+    }
+    return self;
+}
+
+- (void)addPlayer{
+    
+    [[self.fireBaseRef childByAppendingPath:self.playerID] setValue:@{@"playerID": self.playerID}];
+}
+
+- (void)startMonitoringNumberOfPlayers{
+    
+    [self.fireBaseRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+        self.numberOfPlayers =  snapshot.childrenCount;
+    }];
+}
+
+-(void)handleDisconnections{
+    
+
+}
+
+#pragma mark - Helper methods
+
+-(NSString*)createPlayerID{
+    
+    NSUUID *playerUUID = [[UIDevice currentDevice] identifierForVendor];
+    NSString *playerID = [playerUUID UUIDString];
+    
+    return playerID;
+}
+
+@end
